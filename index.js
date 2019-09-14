b091b2e751cca56f74e6b63f4ba8d15802e3ccc5
Kevin Clement <kevin.clement@gmail.com>
2019-09-14 22:21:50 +0100

    updating status location

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let logger = new (require('./logging'))
 let managers = [];
 //managers.push(new (require('./manager.mummy'))({ name: 'mummy', logger: logger, fb: fb }))
 //managers.push(new (require('./manager.hands'))({ name: 'hands', logger: logger, fb: fb }))
-managers.push(new (require('./manager.clock'))({ name: 'clock', logger: logger, fb: fb }))
+//managers.push(new (require('./manager.clock'))({ name: 'clock', logger: logger, fb: fb }))
 
 // might want to turn this off while doing dev, so I have a flag for it
 let ENABLE_FIREBASE_LOGS = true;
@@ -12,7 +12,7 @@ if (ENABLE_FIREBASE_LOGS) {
     logger.enableFirebase(fb.db);
 }
 
-logger.log('pi: Started ExitPuzzles Zoltar server.');
+logger.log('pi: Started ExitPuzzles Mausoleum server.');
 
 // listen for control operations in the db, filter only ops not completed
 fb.db.ref('museum/operations').orderByChild('completed').equalTo(null).on("child_added", function(snapshot) {
@@ -24,14 +24,14 @@ fb.db.ref('museum/operations').orderByChild('completed').equalTo(null).on("child
  });
 
 // update started time and set a ping timer
-fb.db.ref('museum/status').update({
+fb.db.ref('museum/status/mausoleum').update({
     piStarted: (new Date()).toLocaleString(),
     piPing: (new Date()).getTime()
 })
 
 // heartbeat timer
 setInterval(()  => {
-    fb.db.ref('museum/status').update({
+    fb.db.ref('museum/status/mausoleum').update({
         piPing: (new Date()).getTime()
     })
 }, 30000)
