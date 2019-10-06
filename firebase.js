ed30ee70705e226af869e6322b60b0af3dfbd40d
Kevin Clement <kevin.clement@gmail.com>
2019-10-06 14:07:24 -0700

    switch to prod

--- firebase.js
+++ firebase.js
@@ -4,8 +4,8 @@ module.exports = class Firebase {
     constructor(opts) {
 
         // init firebase
-        let config = { databaseURL: "https://exitpuzzles-admin-dev.firebaseio.com" }
-        //let config = { databaseURL: "https://exitpuzzles-admin.firebaseio.com" }
+        // let config = { databaseURL: "https://exitpuzzles-admin-dev.firebaseio.com" }
+        let config = { databaseURL: "https://exitpuzzles-admin.firebaseio.com" }
         this.db = firebase.initializeApp(config).database()
     }
 }
